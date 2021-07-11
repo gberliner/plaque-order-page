61f2bf9dd8a8a07a86e01143ea7dc29dc0d3d0fa
gberliner <guy.berliner@papercut.com>
2021-07-11 00:17:05 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {MapWithPlaceholder} from './map'
 ReactDOM.render(
   <React.StrictMode>
+    <MapWithPlaceholder />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
