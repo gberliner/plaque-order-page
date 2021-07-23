3e0bfe7d25787777131adf8b704e511b155ace43
guy berliner <guy.berliner@gmail.com>
2021-07-23 12:54:30 +0000

    add express wildcard route to fix "Cant GET /" error

--- server.ts
+++ server.ts
@@ -5,6 +5,8 @@ import {createPlaqueOrder} from './api/createOrder';
     import {checkPrice} from './api/checkPrice';
 const app = express();
 app.use(express.static(path.join(import.meta.url, 'build')));
+
+
 app.use(express.json());
 app.get('/ping', function (req, res) {
  return res.send('pong');
@@ -12,4 +14,7 @@ app.get('/ping', function (req, res) {
 
 app.post('/api/process-payment', createPlaqueOrder, processPayment);
 app.get('/api/check-price', checkPrice);
+app.get('*', (request, response) => {
+	response.sendFile(path.join(import.meta.url, 'build', 'index.html'));
+});
 app.listen(process.env.NODE_ENV === "production" ? parseInt(process?.env?.PORT as string): 8080);
