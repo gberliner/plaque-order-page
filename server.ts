96046c0623d0870f67e41943a2f636ed41badc79
guy berliner <guy.berliner@gmail.com>
2021-07-23 13:58:10 +0000

    express not getting right absolute paths from 'import.meta.url' (they need to be translated into actual paths, instead of urls)

--- server.ts
+++ server.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import path from 'path';
 import processPayment from './api/paymentRcvd.js';
 import {createPlaqueOrder} from './api/createOrder';
-    import {checkPrice} from './api/checkPrice';
+import {checkPrice} from './api/checkPrice';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-app.use(express.static(path.join(import.meta.url, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.use(express.json());
@@ -15,6 +19,9 @@ app.get('/ping', function (req, res) {
 app.post('/api/process-payment', createPlaqueOrder, processPayment);
 app.get('/api/check-price', checkPrice);
 app.get('/', (request, response) => {
-	response.sendFile(path.join(import.meta.url, 'build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('/*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(process.env.NODE_ENV === "production" ? parseInt(process?.env?.PORT as string): 8080);
